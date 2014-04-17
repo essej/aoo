4cadc605a26771ae848283de27861707cc1276c7
ritsch <ritsch@iem.at>
2014-04-17 21:34:06 +0200

    jack app for testing aoo_lib

---
+++ jack_aoo/jack_aoo.c
@@ -0,0 +1,9 @@
+/*
+ *  jack_aoo - a jack application implementing aoo drains and sources
+ * 
+ *  Copyright (c) 2014 Winfried Ritsch
+ * 
+ * This library is covered by the GPL, read licences
+ * at <http://www.gnu.org/licenses/>  for details
+ * 
+ */
---
+++ jack_aoo/jack_aoo.h
@@ -0,0 +1,30 @@
+/*
+ *  jack_aoo - a jack application implementing aoo drains and sources
+ * 
+ * Copyright (c) 2014 Winfried Ritsch <ritsch_at_algo.mur.at>
+ * 
+ * This library is covered by the GPL, read licences
+ * at <http://www.gnu.org/licenses/>  for details
+ * 
+ */
+
+#ifndef __AOO_H__
+#define __AOO_H__
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <limits.h>
+/* #include <math.h> */
+
+int aoo_setup(void);         /* initialize lib */
+/*int aoo_release(void);         /* release lib */
+
+/* drains  interface */
+int aoo_drain_new(int id);     /* setup new drain */
+int aoo_drain_start(int id);   /* start processing */
+int aoo_drain_perform(int id); /* start processing */
+int aoo_drain_stop(int id);    /* stop processing */
+int aoo_drain_free(int id);    /* free new drain */
+
+#endif /* __AOO_H__ */
