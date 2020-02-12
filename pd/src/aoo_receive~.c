581ad9e7e5ace7be69af78712665535f101110ba
Christof Ressi <christof.ressi@gmx.at>
2020-02-12 19:09:03 +0100

    add aoo_send~ and aoo_receive~ externals

---
+++ pd/src/aoo_receive~.c
@@ -0,0 +1,396 @@
+#include "m_pd.h"
+#include "aoo/aoo.h"
+
+#include <string.h>
+#include <assert.h>
+#include <pthread.h>
+#include <stdio.h>
+
+#ifdef _WIN32
+#include <winsock2.h>
+typedef int socklen_t;
+#else
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <unistd.h>
+#include <netdb.h>
+#endif
+
+#define DEFBUFSIZE 10
+
+/*////////////////////// socket listener //////////////////*/
+
+typedef struct _client {
+    int socket;
+    struct sockaddr_storage addr;
+    int addrlen;
+} t_client;
+
+typedef struct _aoo_receive t_aoo_receive;
+
+static t_class *socket_listener_class;
+
+typedef struct _socket_listener
+{
+    t_pd pd;
+    t_symbol *sym;
+    // dependants
+    t_aoo_receive **recv;
+    int numrecv; // doubles as refcount
+    // socket
+    int socket;
+    int port;
+    t_client *clients;
+    int numclients;
+    // threading
+    pthread_t thread;
+    pthread_mutex_t mutex;
+    int quit;
+} t_socket_listener;
+
+static void socket_listener_reply(t_client *x, const char *data, int32_t n)
+{
+    // no check or synchronization needed
+    sendto(x->socket, data, n, 0, (const struct sockaddr *)&x->addr, x->addrlen);
+}
+
+static void aoo_receive_handle_message(t_aoo_receive *x, int32_t id,
+                                const char * data, int32_t n, void *src, aoo_replyfn fn);
+
+static void* socket_listener_threadfn(void *y)
+{
+    t_socket_listener *x = (t_socket_listener *)y;
+
+    while (1){
+        struct sockaddr_storage sa;
+        socklen_t len = sizeof(sa);
+        char buf[AOO_MAXPACKETSIZE];
+        int nbytes = recvfrom(x->socket, buf, AOO_MAXPACKETSIZE, 0, (struct sockaddr *)&sa, &len);
+        if (nbytes > 0){
+            t_client *c = 0;
+            // try to find client
+            int n = x->numclients;
+            for (int i = 0; i < n; ++i){
+                if (len == x->clients[i].addrlen &&
+                    !memcmp(&sa, &x->clients[i].addr, len)){
+                    c = &x->clients[i];
+                    break;
+                }
+            }
+            if (!c){
+                // add client
+                x->clients = resizebytes(x->clients, sizeof(t_client) * n, sizeof(t_client) * (n + 1));
+                x->numclients = n + 1;
+                c = &x->clients[n];
+                c->socket = x->socket;
+                memcpy(&c->addr, &sa, len);
+                c->addrlen = len;
+            }
+            // forward OSC packet to matching receivers
+            int32_t id = 0;
+            if (aoo_parsepattern(buf, nbytes, &id) > 0){
+                pthread_mutex_lock(&x->mutex);
+                for (int i = 0; i < x->numrecv; ++i){
+                    aoo_receive_handle_message(x->recv[i], id, buf, nbytes,
+                                               c, (aoo_replyfn)socket_listener_reply);
+                }
+                pthread_mutex_unlock(&x->mutex);
+            } else {
+                // not a valid AoO OSC message
+            }
+        } else if (nbytes < 0){
+            if (!x->quit){
+            #ifdef _WIN32
+                int err = WSAGetLastError();
+            #else
+                int err = errno;
+            #endif
+                fprintf(stderr, "recv: %s (%d)\n", strerror(err), err);
+                fflush(stderr);
+            }
+            break;
+        }
+    }
+
+    return 0;
+}
+
+static int aoo_receive_match(t_aoo_receive *x, t_aoo_receive *other);
+
+t_socket_listener* socket_listener_add(t_aoo_receive *r, int port)
+{
+    // make bind symbol for port number
+    char buf[64];
+    snprintf(buf, sizeof(buf), "__aoo__listener_%d", port);
+    t_symbol *s = gensym(buf);
+    t_socket_listener *x = (t_socket_listener *)pd_findbyclass(s, socket_listener_class);
+    if (x){
+        // check receiver and add to list
+        pthread_mutex_lock(&x->mutex);
+    #if 1
+        for (int i = 0; i < x->numrecv; ++i){
+            if (aoo_receive_match(x->recv[i], r)){
+                pthread_mutex_unlock(&x->mutex);
+                return 0;
+            }
+        }
+    #endif
+        x->recv = (t_aoo_receive **)resizebytes(x->recv, sizeof(t_aoo_receive *) * x->numrecv,
+                                                sizeof(t_aoo_receive *) * (x->numrecv + 1));
+        x->recv[x->numrecv] = r;
+        x->numrecv++;
+        pthread_mutex_unlock(&x->mutex);
+    } else {
+        // make new socket listener
+        x = getbytes(sizeof(t_socket_listener));
+        x->pd = socket_listener_class;
+        x->sym = s;
+        pd_bind(&x->pd, s);
+
+        // add receiver
+        x->recv = (t_aoo_receive **)getbytes(sizeof(t_aoo_receive *));
+        x->recv[0] = r;
+        x->numrecv = 1;
+
+        // bind socket
+        int sock = socket(AF_INET, SOCK_DGRAM, 0);
+        struct sockaddr_in sa;
+        memset(&sa, 0, sizeof(sa));
+        sa.sin_family = AF_INET;
+        sa.sin_addr.s_addr = INADDR_ANY;
+        sa.sin_port = htons(port);
+        if (bind(sock, (const struct sockaddr *)&sa, sizeof(sa)) < 0){
+            error("couldn't bind to port %d", port);
+        #ifdef _WIN32
+            closesocket(sock);
+        #else
+            close(sock);
+        #endif
+            return 0;
+        }
+        x->socket = sock;
+        x->port = port;
+        x->clients = 0;
+        x->numclients = 0;
+
+        // start thread
+        x->quit = 0;
+        pthread_mutex_init(&x->mutex, 0);
+        pthread_create(&x->thread, 0, socket_listener_threadfn, x);
+
+        post("new socket listener on port %d", x->port);
+    }
+    return x;
+}
+
+void socket_listener_release(t_socket_listener *x, t_aoo_receive *r)
+{
+    if (x->numrecv > 0){
+        if (x->numrecv == 1){
+            // last instance
+            pd_unbind(&x->pd, x->sym);
+        #if 0
+            // tell socket to stop receiving
+            shutdown(x->socket, SD_RECEIVE);
+        #endif
+            x->quit = 1;
+            // close socket (will release any blocking recv() call)
+        #ifdef _WIN32
+            closesocket(x->socket);
+        #else
+            close(x->socket);
+        #endif
+            pthread_join(x->thread, 0); // wait for thread
+            pthread_mutex_destroy(&x->mutex);
+            // free memory
+            if (x->numclients){
+                freebytes(x->clients, sizeof(t_client) * x->numclients);
+            }
+            freebytes(x->recv, sizeof(t_aoo_receive*));
+            post("released socket listener on port %d", x->port);
+            freebytes(x, sizeof(*x));
+        } else {
+            // just remove receiver from list
+            int n = x->numrecv;
+            for (int i = 0; i < n; ++i){
+                if (x->recv[i] == r){
+                    memmove(&x->recv[i], x->recv[i + 1], n - (i + 1));
+                    x->recv = (t_aoo_receive **)resizebytes(x->recv, n * sizeof(t_aoo_receive *),
+                                                            (n - 1) * sizeof(t_aoo_receive *));
+                    x->numrecv--;
+                    return;
+                }
+            }
+            bug("socket_listener_release: receiver not found!");
+        }
+    } else {
+        bug("socket_listener_release: negative refcount!");
+    }
+}
+
+void socket_listener_setup(void)
+{
+    socket_listener_class = class_new(gensym("aoo socket listener"), 0, 0,
+                                  sizeof(t_socket_listener), 0, A_NULL);
+}
+
+/*///////////////////// aoo_receive~ ////////////////////*/
+
+static t_class *aoo_receive_class;
+
+typedef struct _aoo_receive
+{
+    t_object x_obj;
+    t_float x_f;
+    aoo_sink *x_aoo_sink;
+    int32_t x_id;
+    t_sample **x_vec;
+    int x_n;
+    t_socket_listener * x_listener;
+    pthread_mutex_t x_mutex;
+} t_aoo_receive;
+
+static int aoo_receive_match(t_aoo_receive *x, t_aoo_receive *other)
+{
+    if (x == other){
+        bug("socket_listener_add: receiver already added!");
+        return 1;
+    }
+    if (x->x_id == other->x_id){
+        post("warning: aoo_receive~ with id %d already exists!", x->x_id);
+        return 1;
+    }
+    return 0;
+}
+
+static void aoo_receive_handle_message(t_aoo_receive *x, int32_t id,
+                                const char * data, int32_t n, void *src, aoo_replyfn fn)
+{
+    if (id == AOO_ID_WILDCARD || id == x->x_id){
+        pthread_mutex_lock(&x->x_mutex);
+        aoo_sink_handlemessage(x->x_aoo_sink, data, n, src, fn);
+        pthread_mutex_unlock(&x->x_mutex);
+    }
+}
+
+static void aoo_receive_buffersize(t_aoo_receive *x, t_floatarg f)
+{
+    pthread_mutex_lock(&x->x_mutex);
+    aoo_sink_setbuffersize(x->x_aoo_sink, f);
+    pthread_mutex_unlock(&x->x_mutex);
+}
+
+void aoo_receive_listen(t_aoo_receive *x, t_floatarg f)
+{
+    int port = f;
+    if (x->x_listener){
+        if (x->x_listener->port == port){
+            return;
+        }
+        // release old listener
+        socket_listener_release(x->x_listener, x);
+    }
+    // add new listener
+    if (port){
+        x->x_listener = socket_listener_add(x, f);
+        if (x->x_listener){
+            post("listening on port %d", x->x_listener->port);
+        }
+    } else {
+        // stop listening
+        x->x_listener = 0;
+    }
+}
+
+static void aoo_receive_process(const aoo_sample **data, int32_t n, t_aoo_receive *x)
+{
+    assert(sizeof(t_sample) == sizeof(aoo_sample));
+    for (int i = 0; i < x->x_n; ++i){
+        memcpy(x->x_vec[i], data[i], sizeof(aoo_sample) * n);
+    }
+}
+
+static t_int * aoo_receive_perform(t_int *w)
+{
+    t_aoo_receive *x = (t_aoo_receive *)(w[1]);
+    int n = (int)(w[2]);
+
+    if (!aoo_sink_process(x->x_aoo_sink)){
+        // output zeros
+        for (int i = 0; i < x->x_n; ++i){
+            memset(x->x_vec[i], 0, sizeof(t_float) * n);
+        }
+    }
+
+    return w + 3;
+}
+
+static void aoo_receive_dsp(t_aoo_receive *x, t_signal **sp)
+{
+    int n = (aoo_bitdepth)sp[0]->s_n;
+    int sr = sp[0]->s_sr;
+
+    for (int i = 0; i < x->x_n; ++i){
+        x->x_vec[i] = sp[i]->s_vec;
+    }
+    aoo_sink_setup(x->x_aoo_sink, x->x_n, sr, n,
+                   (aoo_processfn)aoo_receive_process, x);
+
+    dsp_add(aoo_receive_perform, 2, (t_int)x, (t_int)sp[0]->s_n);
+}
+
+static void * aoo_receive_new(t_symbol *s, int argc, t_atom *argv)
+{
+    t_aoo_receive *x = (t_aoo_receive *)pd_new(aoo_receive_class);
+
+    x->x_f = 0;
+    x->x_listener = 0;
+    pthread_mutex_init(&x->x_mutex, 0);
+
+    // arg #1: ID
+    int id = atom_getfloatarg(0, argc, argv);
+    x->x_id = id >= 0 ? id : 0;
+    x->x_aoo_sink = aoo_sink_new(x->x_id);
+
+    // arg #2: num channels
+    int nchannels = atom_getfloatarg(1, argc, argv);
+    x->x_n = nchannels > 1 ? nchannels : 1;
+
+    // arg #3: port number
+    if (argc > 2){
+        aoo_receive_listen(x, atom_getfloat(argv + 2));
+    }
+
+    // arg #4: buffer size (ms)
+    aoo_receive_buffersize(x, argc > 3 ? atom_getfloat(argv + 3) : DEFBUFSIZE);
+
+    // make signal outlets
+    for (int i = 0; i < x->x_n; ++i){
+        outlet_new(&x->x_obj, &s_signal);
+    }
+    x->x_vec = (t_sample **)getbytes(sizeof(t_sample *) * x->x_n);
+
+    return x;
+}
+
+static void aoo_receive_free(t_aoo_receive *x)
+{
+    if (x->x_listener){
+        socket_listener_release(x->x_listener, x);
+    }
+    // clean up
+    freebytes(x->x_vec, sizeof(t_sample *) * x->x_n);
+    aoo_sink_free(x->x_aoo_sink);
+}
+
+void aoo_receive_tilde_setup(void)
+{
+    socket_listener_setup();
+
+    aoo_receive_class = class_new(gensym("aoo_receive~"), (t_newmethod)(void *)aoo_receive_new,
+        (t_method)aoo_receive_free, sizeof(t_aoo_receive), 0, A_GIMME, A_NULL);
+    class_addmethod(aoo_receive_class, (t_method)aoo_receive_dsp, gensym("dsp"), A_CANT, A_NULL);
+    class_addmethod(aoo_receive_class, (t_method)aoo_receive_listen, gensym("listen"), A_FLOAT, A_NULL);
+    class_addmethod(aoo_receive_class, (t_method)aoo_receive_buffersize,
+                    gensym("bufsize"), A_FLOAT, A_NULL);
+}
---
+++ pd/src/aoo_send~.c
@@ -0,0 +1,297 @@
+#include "m_pd.h"
+#include "aoo/aoo.h"
+
+#include <string.h>
+#include <assert.h>
+#include <pthread.h>
+#include <stdio.h>
+
+#ifdef _WIN32
+#include <winsock2.h>
+typedef int socklen_t;
+#else
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <unistd.h>
+#include <netdb.h>
+#endif
+
+#define DEFBUFSIZE 10
+
+static t_class *aoo_send_class;
+
+typedef struct _aoo_send
+{
+    t_object x_obj;
+    t_float x_f;
+    aoo_source *x_aoo_source;
+    aoo_format x_format;
+    t_float **x_vec;
+    t_atom x_sink_id_arg;
+    int32_t x_sink_id;
+    int32_t x_sink_chn;
+    // socket
+    int x_socket;
+    struct sockaddr_in x_addr;
+    // threading
+    pthread_t x_thread;
+    pthread_cond_t x_cond;
+    pthread_mutex_t x_mutex;
+} t_aoo_send;
+
+static void aoo_send_channel(t_aoo_send *x, t_floatarg f)
+{
+    if (f >= 0){
+        pthread_mutex_lock(&x->x_mutex);
+        aoo_source_setsinkchannel(x->x_aoo_source, x, x->x_sink_id, f);
+        x->x_sink_chn = f;
+        pthread_mutex_unlock(&x->x_mutex);
+    }
+}
+
+static void aoo_send_packetsize(t_aoo_send *x, t_floatarg f)
+{
+    pthread_mutex_lock(&x->x_mutex);
+    aoo_source_setpacketsize(x->x_aoo_source, f);
+    pthread_mutex_unlock(&x->x_mutex);
+}
+
+static void aoo_send_reply(t_aoo_send *x, const char *data, int32_t n)
+{
+    // called while holding the lock (socket might close or address might change!)
+    if (x->x_socket >= 0 && x->x_addr.sin_family == AF_INET){
+        sendto(x->x_socket, data, n, 0, (const struct sockaddr *)&x->x_addr, sizeof(x->x_addr));
+    }
+}
+
+void *aoo_send_threadfn(void *y)
+{
+    t_aoo_send *x = (t_aoo_send *)y;
+
+    pthread_mutex_lock(&x->x_mutex);
+    while (x->x_socket >= 0){
+        // send all available outgoing packets
+        while (aoo_source_send(x->x_aoo_source)) ;
+        // check for pending incoming packets
+        while (1){
+            // non-blocking receive via select()
+            struct timeval tv;
+            tv.tv_sec = 0;
+            tv.tv_usec = 0;
+            fd_set rdset;
+            FD_ZERO(&rdset);
+            FD_SET(x->x_socket, &rdset);
+            if (select(x->x_socket + 1, &rdset, 0, 0, &tv) > 0){
+                if (FD_ISSET(x->x_socket, &rdset)){
+                    // receive packet
+                    char buf[AOO_MAXPACKETSIZE];
+                    int nbytes = recv(x->x_socket, buf, AOO_MAXPACKETSIZE, 0);
+                    if (nbytes > 0){
+                        aoo_source_handlemessage(x->x_aoo_source, buf, nbytes,
+                                                 x, (aoo_replyfn)aoo_send_reply);
+                        continue; // check for more
+                    }
+                }
+            }
+            break;
+        }
+        // wait for more
+        pthread_cond_wait(&x->x_cond, &x->x_mutex);
+    }
+    pthread_mutex_unlock(&x->x_mutex);
+
+    return 0;
+}
+
+static void aoo_send_set(t_aoo_send *x, t_symbol *s, int argc, t_atom *argv)
+{
+    if (argc){
+        pthread_mutex_lock(&x->x_mutex);
+        // remove old sink
+        aoo_source_removeall(x->x_aoo_source);
+        // add new sink
+        if (argv->a_type == A_SYMBOL){
+            if (*argv->a_w.w_symbol->s_name == '*'){
+                aoo_source_addsink(x->x_aoo_source, x, AOO_ID_WILDCARD, (aoo_replyfn)aoo_send_reply);
+            } else {
+                return;
+            }
+            x->x_sink_id = AOO_ID_WILDCARD;
+        } else {
+            int32_t id = atom_getfloat(argv);
+            aoo_source_addsink(x->x_aoo_source, x, id, (aoo_replyfn)aoo_send_reply);
+            x->x_sink_id = id;
+        }
+        pthread_mutex_unlock(&x->x_mutex);
+
+        aoo_send_channel(x, atom_getfloatarg(1, argc, argv));
+    }
+}
+
+static void aoo_send_clear(t_aoo_send *x)
+{
+    pthread_mutex_lock(&x->x_mutex);
+    aoo_source_removeall(x->x_aoo_source);
+    pthread_mutex_unlock(&x->x_mutex);
+}
+
+static t_int * aoo_send_perform(t_int *w)
+{
+    t_aoo_send *x = (t_aoo_send *)(w[1]);
+    int n = (int)(w[2]);
+
+    assert(sizeof(t_sample) == sizeof(aoo_sample));
+
+    if (x->x_addr.sin_family == AF_INET){
+        if (aoo_source_process(x->x_aoo_source, (const aoo_sample **)x->x_vec, n)){
+            pthread_cond_signal(&x->x_cond);
+        }
+    }
+    return w + 3;
+}
+
+static void aoo_send_dsp(t_aoo_send *x, t_signal **sp)
+{
+    pthread_mutex_lock(&x->x_mutex);
+    x->x_format.blocksize = sp[0]->s_n;
+    x->x_format.samplerate = sp[0]->s_sr;
+    aoo_source_setformat(x->x_aoo_source, &x->x_format);
+    pthread_mutex_unlock(&x->x_mutex);
+
+    for (int i = 0; i < x->x_format.nchannels; ++i){
+        x->x_vec[i] = sp[i]->s_vec;
+    }
+
+    dsp_add(aoo_send_perform, 2, (t_int)x, (t_int)sp[0]->s_n);
+}
+
+static void aoo_send_loadbang(t_aoo_send *x, t_floatarg f)
+{
+    // LB_LOAD
+    if (f == 0){
+        if (x->x_sink_id_arg.a_type != A_NULL){
+            // set sink ID
+            aoo_send_set(x, 0, 1, &x->x_sink_id_arg);
+            aoo_send_channel(x, x->x_sink_chn);
+        }
+    }
+}
+
+void aoo_send_disconnect(t_aoo_send *x)
+{
+    pthread_mutex_lock(&x->x_mutex);
+    memset(&x->x_addr, 0, sizeof(x->x_addr));
+    pthread_mutex_unlock(&x->x_mutex);
+}
+
+void aoo_send_connect(t_aoo_send *x, t_symbol *s, int argc, t_atom *argv)
+{
+    t_symbol *hostname = atom_getsymbolarg(0, argc, argv);
+    int port = atom_getfloatarg(1, argc, argv);
+
+    struct hostent *he = gethostbyname(hostname->s_name);
+    if (he){
+        pthread_mutex_lock(&x->x_mutex);
+        memcpy(&x->x_addr.sin_addr, he->h_addr_list[0], he->h_length);
+        x->x_addr.sin_family = AF_INET;
+        x->x_addr.sin_port = htons(port);
+        pthread_mutex_unlock(&x->x_mutex);
+
+        post("connected to %s on port %d", he->h_name, port);
+    } else {
+        aoo_send_disconnect(x);
+
+        error("couldn't resolve hostname '%s'", hostname->s_name);
+    }
+}
+
+static void * aoo_send_new(t_symbol *s, int argc, t_atom *argv)
+{
+    t_aoo_send *x = (t_aoo_send *)pd_new(aoo_send_class);
+
+    memset(&x->x_addr, 0, sizeof(x->x_addr));
+    x->x_socket = socket(AF_INET, SOCK_DGRAM, 0);
+    if (x->x_socket >= 0){
+        int val = 1;
+        if (setsockopt(x->x_socket, SOL_SOCKET, SO_BROADCAST, (void *)&val, sizeof(val))){
+            error("couldn't set SO_BROADCAST");
+        }
+    }
+    pthread_mutex_init(&x->x_mutex, 0);
+    pthread_cond_init(&x->x_cond, 0);
+
+    // arg #1: ID
+    int src = atom_getfloatarg(0, argc, argv);
+    x->x_aoo_source = aoo_source_new(src >= 0 ? src : 0);
+    aoo_source_setbuffersize(x->x_aoo_source, DEFBUFSIZE);
+
+    // arg #2: num channels
+    int nchannels = atom_getfloatarg(1, argc, argv);
+    memset(&x->x_format, 0, sizeof(x->x_format));
+    x->x_format.nchannels = nchannels > 1 ? nchannels : 1;
+    // LATER make this settable
+    x->x_format.mime_type = AOO_MIME_PCM;
+    x->x_format.bitdepth = AOO_FLOAT32;
+
+    // arg #3: sink ID
+    x->x_sink_id = -1;
+    if (argc > 2){
+        x->x_sink_id_arg.a_type = argv[2].a_type;
+        x->x_sink_id_arg.a_w = argv[2].a_w;
+    } else {
+        x->x_sink_id_arg.a_type = A_NULL;
+    }
+
+    // arg #4: sink channel
+    x->x_sink_chn = atom_getfloatarg(3, argc, argv);
+
+    // make additional inlets
+    if (nchannels > 1){
+        while (--nchannels){
+            inlet_new(&x->x_obj, &x->x_obj.ob_pd, &s_signal, &s_signal);
+        }
+    }
+    x->x_vec = (t_sample **)getbytes(sizeof(t_sample *) * x->x_format.nchannels);
+
+    pthread_create(&x->x_thread, 0, aoo_send_threadfn, x);
+
+    return x;
+}
+
+static void aoo_send_free(t_aoo_send *x)
+{
+    pthread_mutex_lock(&x->x_mutex);
+#ifdef _WIN32
+    closesocket(x->x_socket);
+#else
+    close(x->x_socket);
+#endif
+    x->x_socket = -1;
+    pthread_mutex_unlock(&x->x_mutex);
+
+    // notify thread and join
+    pthread_cond_signal(&x->x_cond);
+    pthread_join(x->x_thread, 0);
+
+    pthread_mutex_destroy(&x->x_mutex);
+    pthread_condattr_destroy(&x->x_cond);
+
+    aoo_source_free(x->x_aoo_source);
+
+    freebytes(x->x_vec, sizeof(t_sample *) * x->x_format.nchannels);
+}
+
+void aoo_send_tilde_setup(void)
+{
+    aoo_send_class = class_new(gensym("aoo_send~"), (t_newmethod)(void *)aoo_send_new,
+        (t_method)aoo_send_free, sizeof(t_aoo_send), 0, A_GIMME, A_NULL);
+    CLASS_MAINSIGNALIN(aoo_send_class, t_aoo_send, x_f);
+    class_addmethod(aoo_send_class, (t_method)aoo_send_dsp, gensym("dsp"), A_CANT, A_NULL);
+    class_addmethod(aoo_send_class, (t_method)aoo_send_loadbang, gensym("loadbang"), A_FLOAT, A_NULL);
+    class_addmethod(aoo_send_class, (t_method)aoo_send_connect, gensym("connect"), A_GIMME, A_NULL);
+    class_addmethod(aoo_send_class, (t_method)aoo_send_disconnect, gensym("disconnect"), A_NULL);
+    class_addmethod(aoo_send_class, (t_method)aoo_send_set, gensym("set"), A_GIMME, A_NULL);
+    class_addmethod(aoo_send_class, (t_method)aoo_send_channel, gensym("channel"), A_FLOAT, A_NULL);
+    class_addmethod(aoo_send_class, (t_method)aoo_send_packetsize, gensym("packetsize"), A_FLOAT, A_NULL);
+    class_addmethod(aoo_send_class, (t_method)aoo_send_clear, gensym("clear"), A_NULL);
+}
