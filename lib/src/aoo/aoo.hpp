40fc71db246a8fa8140c67b9703979f1c5a0fb76
Christof Ressi <christof.ressi@gmx.at>
2020-03-16 16:07:57 +0100

    forgot to mark interface methods pure virtual

--- lib/src/aoo/aoo.hpp
+++ lib/src/aoo/aoo.hpp
@@ -18,24 +18,24 @@ public:
 
     static void destroy(isource *x);
 
-    virtual void set_format(aoo_format& f);
+    virtual void set_format(aoo_format& f) = 0;
 
-    virtual void setup(aoo_source_settings& settings);
+    virtual void setup(aoo_source_settings& settings) = 0;
 
-    virtual void add_sink(void *sink, int32_t id, aoo_replyfn fn);
+    virtual void add_sink(void *sink, int32_t id, aoo_replyfn fn) = 0;
 
-    virtual void remove_sink(void *sink, int32_t id);
+    virtual void remove_sink(void *sink, int32_t id) = 0;
 
-    virtual void remove_all();
+    virtual void remove_all() = 0;
 
-    virtual void set_sink_channel(void *sink, int32_t id, int32_t chn);
+    virtual void set_sink_channel(void *sink, int32_t id, int32_t chn) = 0;
 
     virtual void handle_message(const char *data, int32_t n,
-                                void *endpoint, aoo_replyfn fn);
+                                void *endpoint, aoo_replyfn fn) = 0;
 
-    virtual bool send();
+    virtual bool send() = 0;
 
-    virtual bool process(const aoo_sample **data, int32_t n, uint64_t t);
+    virtual bool process(const aoo_sample **data, int32_t n, uint64_t t) = 0;
 
     class deleter {
     public:
@@ -57,12 +57,13 @@ public:
 
     static void destroy(isink *x);
 
-    virtual void setup(aoo_sink_settings& settings);
+    virtual void setup(aoo_sink_settings& settings) = 0;
 
     virtual int32_t handle_message(const char *data, int32_t n,
-                                   void *endpoint, aoo_replyfn fn);
+                                   void *endpoint, aoo_replyfn fn) = 0;
 
-    virtual int32_t process(uint64_t t);
+    virtual int32_t process(uint64_t t) = 0;
+
 
     class deleter {
     public:
