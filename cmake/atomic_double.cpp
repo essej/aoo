49ecc37610e94b6f7a56d9c513d7d0e0ad0a0da4
Christof <christof.ressi@gmx.at>
2021-09-27 17:32:13 +0200

    cmake: restructure + better atomic tests

--- cmake/atomic_double.cpp
+++ cmake/atomic_double.cpp
@@ -1,10 +1,13 @@
 #include <atomic>
+#include <stdio.h>
 
 volatile std::atomic<double> d{0};
 
+#ifdef REQUIRE_ALWAYS_LOCKFREE
 #if __cplusplus >= 201703L
 static_assert(std::atomic<double>::is_always_lock_free,
               "atomic double is not lockfree!");
+#endif
 #endif
 
 int main() {
--- cmake/atomic_int64.cpp
+++ cmake/atomic_int64.cpp
@@ -3,9 +3,11 @@
 
 volatile std::atomic<int64_t> i{0};
 
+#ifdef REQUIRE_ALWAYS_LOCKFREE
 #if __cplusplus >= 201703L
 static_assert(std::atomic<int64_t>::is_always_lock_free,
               "atomic int64_t is not lockfree!");
+#endif
 #endif
 
 int main() {
