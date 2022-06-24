9b146b7c2e77cc43b17890d3bb7b0a5f492b1d91
Christof Ressi <info@christofressi.com>
2022-06-25 01:29:24 +0200

    pcm: add 8-bit support

--- pd/src/aoo_common.cpp
+++ pd/src/aoo_common.cpp
@@ -97,6 +97,9 @@ bool format_parse(t_pd *x, AooFormatStorage &f, int argc, t_atom *argv,
         auto nbits = format_getparam(x, argc, argv, 4, "bitdepth", 4);
         AooPcmBitDepth bitdepth;
         switch (nbits){
+        case 1:
+            bitdepth =  kAooPcmInt8;
+            break;
         case 2:
             bitdepth = kAooPcmInt16;
             break;
@@ -172,25 +175,28 @@ int format_to_atoms(const AooFormat &f, int argc, t_atom *argv)
             return 0;
         }
         auto& fmt = (AooFormatPcm &)f;
-        int nbits;
+        int nbytes;
         switch (fmt.bitDepth){
+        case kAooPcmInt8:
+            nbytes = 1;
+            break;
         case kAooPcmInt16:
-            nbits = 2;
+            nbytes = 2;
             break;
         case kAooPcmInt24:
-            nbits = 3;
+            nbytes = 3;
             break;
         case kAooPcmFloat32:
-            nbits = 4;
+            nbytes = 4;
             break;
         case kAooPcmFloat64:
-            nbits = 8;
+            nbytes = 8;
             break;
         default:
             pd_error(0, "format_to_atoms: bad bitdepth argument %d", fmt.bitDepth);
             return 0;
         }
-        SETFLOAT(argv + 4, nbits);
+        SETFLOAT(argv + 4, nbytes);
         return 5;
     }
 #if USE_CODEC_OPUS
